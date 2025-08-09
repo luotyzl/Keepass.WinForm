40aea4413a00dc3232a2886179b3ec813283a603
RaymonL <luotyzl@gmail.com>
2025-08-10 00:32:27 +1200

    progress

--- Keepass.WinForm/Main.Designer.cs
+++ Keepass.WinForm/Main.Designer.cs
@@ -20,6 +20,14 @@ partial class Main
         base.Dispose(disposing);
     }
 
+    private System.Windows.Forms.TreeView groupsTreeView;
+    private System.Windows.Forms.ListView entriesListView;
+    private System.Windows.Forms.Panel detailsPanel;
+    private System.Windows.Forms.Panel centerPanel;
+    private System.Windows.Forms.TextBox searchTextBox;
+    private System.Windows.Forms.Splitter splitter1;
+    private System.Windows.Forms.Splitter splitter2;
+
     #region Windows Form Designer generated code
 
     /// <summary>
@@ -28,16 +36,94 @@ partial class Main
     /// </summary>
     private void InitializeComponent()
     {
+        this.groupsTreeView = new System.Windows.Forms.TreeView();
+        this.entriesListView = new System.Windows.Forms.ListView();
+        this.detailsPanel = new System.Windows.Forms.Panel();
+        this.centerPanel = new System.Windows.Forms.Panel();
+        this.searchTextBox = new System.Windows.Forms.TextBox();
+        this.splitter1 = new System.Windows.Forms.Splitter();
+        this.splitter2 = new System.Windows.Forms.Splitter();
+        this.centerPanel.SuspendLayout();
         this.SuspendLayout();
+        // 
+        // groupsTreeView
+        // 
+        this.groupsTreeView.Dock = System.Windows.Forms.DockStyle.Left;
+        this.groupsTreeView.Location = new System.Drawing.Point(0, 0);
+        this.groupsTreeView.Name = "groupsTreeView";
+        this.groupsTreeView.Size = new System.Drawing.Size(200, 600);
+        this.groupsTreeView.TabIndex = 0;
+        // 
+        // splitter1
+        // 
+        this.splitter1.Location = new System.Drawing.Point(200, 0);
+        this.splitter1.Name = "splitter1";
+        this.splitter1.Size = new System.Drawing.Size(3, 600);
+        this.splitter1.TabIndex = 1;
+        this.splitter1.TabStop = false;
+        // 
+        // centerPanel
+        // 
+        this.centerPanel.Controls.Add(this.entriesListView);
+        this.centerPanel.Controls.Add(this.searchTextBox);
+        this.centerPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+        this.centerPanel.Location = new System.Drawing.Point(203, 0);
+        this.centerPanel.Name = "centerPanel";
+        this.centerPanel.Size = new System.Drawing.Size(300, 600);
+        this.centerPanel.TabIndex = 5;
+        // 
+        // searchTextBox
+        // 
+        this.searchTextBox.Dock = System.Windows.Forms.DockStyle.Top;
+        this.searchTextBox.Location = new System.Drawing.Point(0, 0);
+        this.searchTextBox.Name = "searchTextBox";
+        this.searchTextBox.PlaceholderText = "Search entries...";
+        this.searchTextBox.Size = new System.Drawing.Size(300, 23);
+        this.searchTextBox.TabIndex = 0;
+        this.searchTextBox.TextChanged += new System.EventHandler(this.searchTextBox_TextChanged);
+        // 
+        // entriesListView
+        // 
+        this.entriesListView.Dock = System.Windows.Forms.DockStyle.Fill;
+        this.entriesListView.Location = new System.Drawing.Point(0, 23);
+        this.entriesListView.Name = "entriesListView";
+        this.entriesListView.Size = new System.Drawing.Size(300, 577);
+        this.entriesListView.TabIndex = 1;
+        this.entriesListView.UseCompatibleStateImageBehavior = false;
+        this.entriesListView.View = System.Windows.Forms.View.Details;
+        // 
+        // splitter2
+        // 
+        this.splitter2.Dock = System.Windows.Forms.DockStyle.Right;
+        this.splitter2.Location = new System.Drawing.Point(503, 0);
+        this.splitter2.Name = "splitter2";
+        this.splitter2.Size = new System.Drawing.Size(3, 600);
+        this.splitter2.TabIndex = 3;
+        this.splitter2.TabStop = false;
+        // 
+        // detailsPanel
+        // 
+        this.detailsPanel.Dock = System.Windows.Forms.DockStyle.Right;
+        this.detailsPanel.Location = new System.Drawing.Point(506, 0);
+        this.detailsPanel.Name = "detailsPanel";
+        this.detailsPanel.Size = new System.Drawing.Size(294, 600);
+        this.detailsPanel.TabIndex = 4;
         // 
         // Main
         // 
         this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize = new System.Drawing.Size(800, 600);
+        this.Controls.Add(this.centerPanel);
+        this.Controls.Add(this.splitter2);
+        this.Controls.Add(this.detailsPanel);
+        this.Controls.Add(this.splitter1);
+        this.Controls.Add(this.groupsTreeView);
         this.Name = "Main";
         this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
         this.Text = "Main";
+        this.centerPanel.ResumeLayout(false);
+        this.centerPanel.PerformLayout();
         this.ResumeLayout(false);
     }
 
--- Keepass.WinForm/Main.cs
+++ Keepass.WinForm/Main.cs
@@ -2,8 +2,96 @@ namespace Keepass.WinForm;
 
 public partial class Main : Form
 {
+    private List<ListViewItem> allEntries;
+
     public Main()
     {
         InitializeComponent();
+        InitializeTreeView();
+        InitializeListView();
+        LoadSampleData();
+    }
+
+    private void InitializeTreeView()
+    {
+        groupsTreeView.BeginUpdate();
+        
+        TreeNode rootNode = new TreeNode("Database");
+        TreeNode generalNode = new TreeNode("General");
+        TreeNode workNode = new TreeNode("Work");
+        TreeNode personalNode = new TreeNode("Personal");
+        
+        rootNode.Nodes.Add(generalNode);
+        rootNode.Nodes.Add(workNode);
+        rootNode.Nodes.Add(personalNode);
+        
+        groupsTreeView.Nodes.Add(rootNode);
+        rootNode.Expand();
+        
+        groupsTreeView.EndUpdate();
+    }
+
+    private void InitializeListView()
+    {
+        entriesListView.Columns.Add("Title", 150);
+        entriesListView.Columns.Add("Username", 120);
+        entriesListView.Columns.Add("URL", 200);
+        entriesListView.Columns.Add("Modified", 100);
+        
+        entriesListView.FullRowSelect = true;
+        entriesListView.GridLines = true;
+        
+        allEntries = new List<ListViewItem>();
+    }
+
+    private void LoadSampleData()
+    {
+        var sampleEntries = new[]
+        {
+            new { Title = "Gmail", Username = "user@gmail.com", URL = "https://gmail.com", Modified = "2024-01-15" },
+            new { Title = "Facebook", Username = "myusername", URL = "https://facebook.com", Modified = "2024-01-10" },
+            new { Title = "GitHub", Username = "developer", URL = "https://github.com", Modified = "2024-01-20" },
+            new { Title = "Microsoft", Username = "work@company.com", URL = "https://microsoft.com", Modified = "2024-01-08" },
+            new { Title = "Banking", Username = "account123", URL = "https://mybank.com", Modified = "2024-01-12" },
+            new { Title = "Amazon", Username = "shopper", URL = "https://amazon.com", Modified = "2024-01-18" }
+        };
+
+        foreach (var entry in sampleEntries)
+        {
+            var item = new ListViewItem(new[] { entry.Title, entry.Username, entry.URL, entry.Modified });
+            allEntries.Add(item);
+            entriesListView.Items.Add(item);
+        }
+    }
+
+    private void searchTextBox_TextChanged(object sender, EventArgs e)
+    {
+        FilterEntries(searchTextBox.Text);
+    }
+
+    private void FilterEntries(string searchText)
+    {
+        entriesListView.Items.Clear();
+        
+        if (string.IsNullOrWhiteSpace(searchText))
+        {
+            foreach (var item in allEntries)
+            {
+                entriesListView.Items.Add((ListViewItem)item.Clone());
+            }
+        }
+        else
+        {
+            var filteredEntries = allEntries.Where(item =>
+                item.SubItems[0].Text.IndexOf(searchText, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                item.SubItems[1].Text.IndexOf(searchText, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                item.SubItems[2].Text.IndexOf(searchText, StringComparison.OrdinalIgnoreCase) >= 0
+            );
+
+            foreach (var item in filteredEntries)
+            {
+                entriesListView.Items.Add((ListViewItem)item.Clone());
+            }
+        }
     }
 }
